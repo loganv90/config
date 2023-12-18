27eeb02a60ef505928f1095492fbb33d75f9a0ec
Logan Vaughan <logan.v90@gmail.com>
2023-12-18 00:07:42 -0600

    first commit

---
+++ README.md
@@ -0,0 +1,17 @@
+# config files
+
+includes config files for:
+- alacritty
+- tmux
+- nvim
+
+prerequisites:
+- nvm
+- ripgrep
+- go dlv
+- debugpy
+
+installation:
+- clone into ~/.config/
+- run :Copilot setup
+- make a debugpy virtualenv in ~/.virtualenvs/
---
+++ nvim/lua/logan/init.lua
@@ -0,0 +1,414 @@
+vim.opt.number = true
+vim.opt.relativenumber = true
+
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.expandtab = true
+
+vim.opt.wrap = true
+vim.opt.breakindent = true
+
+vim.opt.incsearch = true
+vim.opt.hlsearch = true
+
+vim.opt.scrolloff = 8
+vim.opt.termguicolors = true
+vim.opt.signcolumn = 'yes'
+vim.opt.updatetime = 80
+
+vim.opt.completeopt = 'menu,menuone,preview'
+
+vim.g.mapleader = ' '
+
+
+
+
+
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+    vim.fn.system({
+        "git",
+        "clone",
+        "--filter=blob:none",
+        "https://github.com/folke/lazy.nvim.git",
+        "--branch=stable",
+        lazypath,
+    }) end
+vim.opt.rtp:prepend(lazypath)
+
+require("lazy").setup({
+    { 'folke/tokyonight.nvim' },
+    { 'folke/neodev.nvim' },
+
+    { 'nvim-lua/plenary.nvim' },
+
+    {
+        'nvim-telescope/telescope.nvim',
+        tag = '0.1.4',
+    },
+
+    {
+        'nvim-treesitter/nvim-treesitter',
+        build = ':TSUpdate',
+    },
+    { 'nvim-treesitter/nvim-treesitter-context' },
+
+    { 'lewis6991/gitsigns.nvim' },
+
+    { 'neovim/nvim-lspconfig' },
+    { 'williamboman/mason.nvim' },
+    { 'williamboman/mason-lspconfig.nvim' },
+
+    { 'hrsh7th/nvim-cmp' },
+    { 'hrsh7th/cmp-buffer' },
+    { 'hrsh7th/cmp-path' },
+    { 'saadparwaiz1/cmp_luasnip' },
+    { 'hrsh7th/cmp-nvim-lsp' },
+    { 'hrsh7th/cmp-nvim-lua' },
+
+    { 'L3MON4D3/LuaSnip' },
+    { 'rafamadriz/friendly-snippets' },
+
+    { 'github/copilot.vim' },
+
+    { 'mfussenegger/nvim-dap' },
+    { 'leoluz/nvim-dap-go' },
+    { 'rcarriga/nvim-dap-ui' },
+})
+
+
+
+
+
+require('neodev').setup()
+require('tokyonight').setup({
+    style = 'night',
+})
+vim.cmd('colorscheme tokyonight')
+
+
+
+
+
+local actions = require('telescope.actions')
+require('telescope').setup({
+    defaults = {
+        mappings = {
+            i = {
+                ['<C-y>'] = function(prompt_bufnr)
+                    for _ = 1, 12 do
+                        actions.move_selection_previous(prompt_bufnr)
+                    end
+                end,
+                ['<C-e>'] = function(prompt_bufnr)
+                    for _ = 1, 12 do
+                        actions.move_selection_next(prompt_bufnr)
+                    end
+                end,
+            },
+        },
+        layout_strategy = 'horizontal',
+        layout_config = {
+            horizontal = {
+                width = 0.9,
+                height = 0.9,
+                preview_width = 0.5,
+                preview_cutoff = 120,
+            },
+        },
+        path_display = {
+            'truncate'
+        }
+    },
+})
+local builtin = require('telescope.builtin')
+vim.keymap.set('n', '<leader>sf', builtin.find_files, {})
+vim.keymap.set('n', '<leader>sg', builtin.live_grep, {})
+vim.keymap.set('n', '<leader>sw', builtin.grep_string, {})
+vim.keymap.set('n', '<leader>sb', builtin.buffers, {})
+vim.keymap.set('n', '<leader>so', builtin.oldfiles, {})
+vim.keymap.set('n', '<leader>sk', builtin.keymaps, {})
+vim.keymap.set('n', '<leader>sd', builtin.diagnostics, {})
+vim.keymap.set('n', '<leader>ss', builtin.git_status, {})
+vim.keymap.set('n', '<leader>sm', builtin.marks, {})
+
+
+
+
+
+require'nvim-treesitter.configs'.setup({
+    ensure_installed = {
+        "c",
+        "lua",
+        "vim",
+        "vimdoc",
+        "query",
+        "go",
+        "javascript",
+        "typescript",
+        "rust",
+        "html",
+        "css",
+        "tsx",
+        "json",
+        "python",
+    },
+    highlight = {
+        enable = true,
+    },
+})
+require'treesitter-context'.setup()
+
+
+
+
+
+require('gitsigns').setup({
+    on_attach = function(bufnr)
+        local gs = package.loaded.gitsigns
+
+        vim.keymap.set(
+            'n',
+            '[c',
+            function()
+                if vim.wo.diff then return '[c' end
+                vim.schedule(function() gs.prev_hunk() end)
+                return '<Ignore>'
+            end,
+            { expr = true, buffer = bufnr, }
+        )
+
+        vim.keymap.set(
+            'n',
+            ']c',
+            function()
+                if vim.wo.diff then return ']c' end
+                vim.schedule(function() gs.next_hunk() end)
+                return '<Ignore>'
+            end,
+            { expr = true, buffer = bufnr }
+        )
+
+        vim.keymap.set('n', '<leader>gr', gs.reset_hunk, { buffer = bufnr })
+        vim.keymap.set('n', '<leader>gp', gs.preview_hunk, { buffer = bufnr })
+    end,
+})
+
+
+
+
+
+local lspconfig = require('lspconfig')
+local cmp_nvim_lsp = require('cmp_nvim_lsp')
+local mason = require('mason')
+local mason_lspconfig = require('mason-lspconfig')
+local cmp = require('cmp')
+local luasnip = require('luasnip')
+local from_vscode = require('luasnip.loaders.from_vscode')
+
+local servers = {
+    tsserver = {},
+    gopls = {},
+    pyright = {},
+    lua_ls = {
+        Lua = {
+            workspace = {
+                checkThirdParty = false,
+            },
+        },
+    },
+}
+local on_attach = function(_, bufnr)
+    vim.keymap.set('n', '<leader>ld', builtin.lsp_definitions, { buffer = bufnr, remap = false })
+    vim.keymap.set('n', '<leader>lr', builtin.lsp_references, { buffer = bufnr, remap = false })
+    vim.keymap.set('n', '<leader>li', builtin.lsp_implementations, { buffer = bufnr, remap = false })
+    vim.keymap.set('n', '<leader>ls', builtin.lsp_document_symbols, { buffer = bufnr, remap = false })
+    vim.keymap.set('n', '<leader>la', vim.lsp.buf.code_action, { buffer = bufnr, remap = false })
+    vim.keymap.set('n', '<leader>ln', vim.lsp.buf.rename, { buffer = bufnr, remap = false })
+    vim.keymap.set('n', '<leader>lh', vim.lsp.buf.hover, { buffer = bufnr, remap = false })
+    vim.keymap.set('i', '<C-k>', vim.lsp.buf.signature_help, { buffer = bufnr, remap = false })
+    vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, { buffer = bufnr, remap = false })
+    vim.keymap.set('n', ']d', vim.diagnostic.goto_next, { buffer = bufnr, remap = false })
+    vim.keymap.set('n', '<leader>lf', vim.diagnostic.open_float, { buffer = bufnr, remap = false })
+end
+local capabilities = cmp_nvim_lsp.default_capabilities()
+
+mason.setup()
+mason_lspconfig.setup {
+    ensure_installed = vim.tbl_keys(servers)
+}
+mason_lspconfig.setup_handlers {
+    function(server_name)
+        lspconfig[server_name].setup {
+            capabilities = capabilities,
+            on_attach = on_attach,
+            settings = servers[server_name],
+        }
+    end,
+}
+
+from_vscode.lazy_load()
+luasnip.config.setup()
+cmp.setup({
+    snippet = {
+        expand = function(args)
+            luasnip.lsp_expand(args.body)
+        end
+    },
+    mapping = cmp.mapping.preset.insert({
+        ['<C-n>'] = cmp.mapping.select_next_item(),
+        ['<C-p>'] = cmp.mapping.select_prev_item(),
+        ['<C-y>'] = cmp.mapping.confirm({ select = true }),
+        ['<C-Space>'] = cmp.mapping.complete(),
+        ['<C-e>'] = cmp.mapping.abort(),
+        ['<C-t>'] = cmp.mapping.scroll_docs(4),
+        ['<C-d>'] = cmp.mapping.scroll_docs(-4),
+    }),
+    sources = {
+        { name = 'nvim_lsp' },
+        { name = 'nvim_lua' },
+        { name = 'luasnip' },
+        { name = 'buffer' },
+        { name = 'path' },
+    },
+})
+
+
+
+
+
+local dap = require('dap')
+local dap_go = require('dap-go')
+local dapui = require('dapui')
+
+vim.keymap.set('n', '<leader>dc', dap.continue, {})
+vim.keymap.set('n', '<leader>ds', dap.step_over, {})
+vim.keymap.set('n', '<leader>di', dap.step_into, {})
+vim.keymap.set('n', '<leader>do', dap.step_out, {})
+vim.keymap.set('n', '<leader>db', dap.toggle_breakpoint, {})
+vim.keymap.set('n', '<leader>dr', dap.run_to_cursor, {})
+vim.keymap.set('n', '<leader>de', dap.terminate, {})
+vim.keymap.set('n', '<leader>dl', dap.run_last, {})
+vim.keymap.set('n', '<leader>du', dap.up, {})
+vim.keymap.set('n', '<leader>dd', dap.down, {})
+vim.keymap.set('n', '<leader>dt', function() dapui.toggle( { reset = true } ) end, {})
+vim.keymap.set('n', '<leader>dg', dap_go.debug_test, {})
+
+dapui.setup({
+    controls = {
+        element = "repl",
+        enabled = true,
+        icons = {
+            disconnect = "dc",
+            pause = "ps",
+            play = "pl",
+            run_last = "rl",
+            step_back = "sb",
+            step_into = "si",
+            step_out = "so",
+            step_over = "st",
+            terminate = "tm",
+        },
+    },
+    icons = {
+        expanded = ">",
+        collapsed = "v",
+        current_frame = ">",
+    },
+    layouts = {
+        {
+            elements = {
+                {
+                    id = "breakpoints",
+                    size = 0.15,
+                },
+                {
+                    id = "console",
+                    size = 0.15,
+                },
+                {
+                    id = "repl",
+                    size = 0.35,
+                },
+                {
+                    id = "stacks",
+                    size = 0.35,
+                },
+            },
+            position = "right",
+            size = 0.30,
+        },
+        {
+            elements = {
+                {
+                    id = "watches",
+                    size = 0.50,
+                },
+                {
+                    id = "scopes",
+                    size = 0.50,
+                },
+            },
+            position = "bottom",
+            size = 0.30,
+        },
+    },
+})
+dap_go.setup()
+
+dap.configurations.python = {
+    {
+        type = 'pythonExecutable',
+        request = 'launch',
+        name = 'Launch',
+        program = '${file}',
+        pythonPath = '${workspaceFolder}/venv/bin/python',
+    },
+    {
+        type = 'pythonServer',
+        request = 'attach',
+        name = 'Attach',
+        host = '127.0.0.1',
+        port = 5678,
+    },
+    {
+        type = 'pythonExecutable',
+        request = 'launch',
+        name = 'Launch unittest',
+        module = 'unittest',
+        args = function() return { '${fileBasenameNoExtension}.' .. vim.fn.input('Test name: ') } end,
+        pythonPath = '${workspaceFolder}/venv/bin/python',
+    },
+}
+
+dap.adapters.pythonExecutable = {
+    type = 'executable',
+    command = os.getenv('HOME') .. '/.virtualenvs/debugpy/bin/python',
+    args = { '-m', 'debugpy.adapter' },
+    enrich_config = function(config, on_config)
+        if not config.pythonPath and not config.python then
+            config.pythonPath = os.getenv('HOME') .. '/.virtualenvs/debugpy/bin/python'
+        end
+        on_config(config)
+    end,
+    options = {
+        source_filetype = 'python',
+    },
+}
+
+dap.adapters.pythonServer = {
+    type = 'server',
+    host = '127.0.0.1',
+    port = 5678,
+    enrich_config = function(config, on_config)
+        if not config.pythonPath and not config.python then
+            config.pythonPath = os.getenv('HOME') .. '/.virtualenvs/debugpy/bin/python'
+        end
+        on_config(config)
+    end,
+    options = {
+        source_filetype = 'python',
+    },
+}
+
